765132cbc7451a2e5aae69985badb27ff7beb925
David Lavelle <davidlavelle1@hotmail.com>
2018-05-30 21:31:01 +0100

    configure postgres db

--- uo_db_api/dev_settings.py
+++ uo_db_api/dev_settings.py
@@ -1,3 +1,11 @@
 from .settings import *
 
 # Overwrite dev settings here . . .
+
+# sqlite db instead of postgres
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- uo_db_api/settings.py
+++ uo_db_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,13 +75,10 @@ WSGI_APPLICATION = 'uo_db_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# Parse database configuration from Heroku's $DATABASE_URL (Postgres)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=500, ssl_require=True)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -118,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku (DATABASE_URL and all that).
+django_heroku.settings(locals())
