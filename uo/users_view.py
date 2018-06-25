f814728a3aa90bdee1d5d0a701d6c3c9a58b040b
David Lavelle <davidlavelle1@hotmail.com>
2018-06-25 21:27:05 +0100

    added flake8

--- uo/users_view.py
+++ uo/users_view.py
@@ -3,6 +3,7 @@ from uo.serializers import UserSerializer
 from uo.permissions import UsersAccessPermission
 
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 
 
@@ -10,6 +11,10 @@ class Users(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (UsersAccessPermission,)
+
+    def post(self, request):
+        # TODO: Check user exists
+        return super().post(request)
 
 
 class User(generics.RetrieveAPIView):
